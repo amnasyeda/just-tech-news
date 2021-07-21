4fee3c090db84ba7ae4e0b3c609b0675b77476c8
Amna Syeda <amna.syeda98@gmail.com>
2021-07-21 11:10:16 -0400

    added seeds

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-// Server for the Just Tech News project
-
-// Dependencies
-// path module
 const path = require('path');
-// dotenv file for sensitive configuration information
-require('dotenv').config();
-// Express.js server
 const express = require('express');
-// All routes as defined in the controllers folder
-const routes = require('./controllers/');
-// Sequelize connection to the database
-const sequelize = require('./config/connection');
-// Handlebars template engine for front-end
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-// Express session to handle session cookies
-const session = require('express-session')
-// Sequelize store to save the session so the user can remain logged in
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// Handlebars helpers
-const helpers = require('./utils/helpers');
 
-// Initialize handlebars for the html templates, using the custom helpers
-const hbs = exphbs.create({ helpers });
-
-// Initialize session with options per best practices.  
-//The secret is defined in the .env file so it is kept secure, along with the mysql login information used in config/connection
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -35,31 +19,21 @@ const sess = {
   })
 };
 
-// Initialize the server
-const app = express();
-// Define the port for the server
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
-// Give the server a path to the public directory for static files
-app.use(express.static(path.join(__dirname, 'public')));
+const helpers = require('./utils/helpers');
 
-// Set handlebars as the template engine for the server
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Have Express parse JSON and string data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Tell the app to use Express Session for the session handling
-app.use(session(sess));
+app.use(require('./controllers/'));
 
-// Give the server the path to the routes
-app.use(routes);
-
-// Turn on connection to db and then to the server
-// force: true to reset the database and clear all values, updating any new relationships
-// force: false to maintain data - aka normal operation
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
